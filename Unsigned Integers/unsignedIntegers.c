65e2e7f63d2eceafbc8aa83b8604b7a0efdabddf
levilu800b <levilu800b@gmail.com>
2023-09-14 12:34:48 +0100

    unsigned integers

---
+++ Unsigned Integers/unsignedIntegers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+    unsigned char iAmUnsignedChar = 0; 
+    unsigned short iAmUnsignedShort = 0;
+    unsigned int iAmUnsignedInt = 0;
+    unsigned long long iAmUnsignedLongLong = 0;
+
+    int n0 = 100;
+    int n1 = 3;
+    // For Addition
+    // int n2 = n0 + n1;
+
+    // For Subtraction
+    // int n2 = n0 - n1;
+
+    // For Multiplication
+    // int n2 = n0 * n1;
+
+    // For Division
+    // int n2 = n0 / n1;
+
+    // For Modulus
+    int n2 = n0 % n1;
+
+    printf("%d\n", n2);
+
+    return 0;
+}
