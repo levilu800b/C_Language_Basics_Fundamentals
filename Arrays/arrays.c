7c77e259db5e3ff3d8c56481173c25539c273321
levilu800b <levilu800b@gmail.com>
2023-09-16 15:32:19 +0100

    Arrays in C

---
+++ Arrays/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+
+    int grades[100];
+    grades[0] = 12;
+
+    printf("%d\n", grades[0]);
+
+    int grades2[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    printf("%d\n", grades2[4]);
+
+    return 0;
+}
