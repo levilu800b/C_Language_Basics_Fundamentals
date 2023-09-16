ab49f4a6677bff7116bac2d69b239a31c283f06f
levilu800b <levilu800b@gmail.com>
2023-09-16 15:40:41 +0100

    strings part 1

---
+++ Strings - part 1/strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char *string = "Hello I am a string";
+    // char *sentence = "Hello I am a string";
+    printf("%s\n", string);
+
+    char c = getchar();
+    printf("%c\n", c);
+    
+    return 0;
+}
