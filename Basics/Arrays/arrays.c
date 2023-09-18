4daddc930e45e2ca758f7cfe7dfff75beb9ab78e
levilu800b <levilu800b@gmail.com>
2023-09-18 12:39:50 +0100

    Array

--- Basics/Arrays/arrays.c
+++ Basics/Arrays/arrays.c
@@ -12,3 +12,19 @@ int main () {
 
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int grades[3]; // Define the grades variable here
+
+//     grades[0] = 80;
+//     grades[1] = 85;
+//     grades[2] = 90;
+
+//     int average = (grades[0] + grades[1] + grades[2]) / 3;
+//     printf("The average of the 3 grades is: %d", average);
+
+//     return 0;
+// }
