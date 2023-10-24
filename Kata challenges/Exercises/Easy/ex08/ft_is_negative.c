f6aaf4fc1b81dfb9f4bd77db7c14f10d8f8fe81b
levilu800b <levilu800b@gmail.com>
2023-10-24 12:39:53 +0100

    easy steps

---
+++ Kata challenges/Exercises/Easy/ex08/ft_is_negative.c
@@ -0,0 +1,29 @@
+// Create a function that displays ’N’ or ’P’ depending on the integer’s sign entered as a parameter.If n is negative,
+// display ’N’.If n is positive or null, display ’P’.
+// • Here’s how it should be prototyped : 
+// void ft_is_negative(int n);
+
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_is_negative(int n)
+{
+    if(n < 0)
+    {
+        ft_putchar('N');
+    }
+    else
+    {
+        ft_putchar('P');
+    }
+}
+
+int main(void)
+{
+    ft_is_negative(-2);
+    return (0);
+}
