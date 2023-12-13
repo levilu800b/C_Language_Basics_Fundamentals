a5b7d8e76457504a177895fcc948d9d378c7d582
levilu800b <levilu800b@gmail.com>
2023-12-13 15:33:41 +0000

    refactor

--- Fundaments/Intermediate 001/C00/ft_is_negative 05/ft_is_negative.c
+++ Fundaments/Intermediate 001/C00/ft_is_negative 05/ft_is_negative.c
@@ -5,16 +5,22 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
+// void    ft_is_negative(int n)
+// {
+//     if(n < 0)
+//     {
+//         ft_putchar('N');
+//     }
+//     else
+//     {
+//         ft_putchar('P');
+//     }
+// }
+
 void    ft_is_negative(int n)
 {
-    if(n < 0)
-    {
-        ft_putchar('N');
-    }
-    else
-    {
-        ft_putchar('P');
-    }
+    x = n < 0 ? 'N' : 'P'
+    ft_putchar(x);
 }
 
 int main(void)
