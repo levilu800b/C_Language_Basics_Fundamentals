01447930b0b286577d76e4322ef0819eda0d162f
levilu800b <levilu800b@gmail.com>
2024-03-29 19:34:57 +0000

    ft  print comb

--- Fundaments/Intermediate 001/C00/ft_print_comb 06/ft_print_comb.c
+++ Fundaments/Intermediate 001/C00/ft_print_comb 06/ft_print_comb.c
@@ -1,8 +1,34 @@
 #include <unistd.h>
-
-void    ft_putchar(char c)
+void ft_print_comb(void)
 {
-    write(1, &c, 1);
+    int a;
+    int b;
+    int c;
+
+    a = '0';
+    while (a <= '7')
+    {
+        b = a + 1;
+        while (b <= '8')
+        {
+            c = b + 1;
+            while (c <= '9')
+            {
+                write(1, &a, 1);
+                write(1, &b, 1);
+                write(1, &c, 1);
+                if (a != '7')
+                    write(1, ", ", 2);
+                c++;
+            }
+            b++;
+        }
+        a++;
+    }
 }
 
-void    
+int main()
+{
+    ft_print_comb();
+    return (0);
+}
