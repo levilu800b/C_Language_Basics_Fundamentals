81921f91ab6fcfb6327a251d7e28cf4559004a6d
levilu800b <levilu800b@gmail.com>
2025-02-06 15:33:57 +0000

    input value

---
+++ Basics/Basics_2/Starter/Starter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int myFavoriteNumber = 0;
+
+    printf("Please Enter your Favorite Number?\n");
+    scanf("%d", &myFavoriteNumber);
+    printf("You enter number: %d\n", myFavoriteNumber);
+    
+    return(0);
+}
