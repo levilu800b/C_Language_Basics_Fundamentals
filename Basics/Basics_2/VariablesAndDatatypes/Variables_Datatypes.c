9cba0e5f4e2ace456090aa806c3a5322e6c149fd
levilu800b <levilu800b@gmail.com>
2025-02-21 15:10:54 +0000

    handling floating numbers using format specifier.

--- Basics/Basics_2/VariablesAndDatatypes/Variables_Datatypes.c
+++ Basics/Basics_2/VariablesAndDatatypes/Variables_Datatypes.c
@@ -7,13 +7,14 @@ int main(void)
     float floatingVar = 331.79;
     double doubleVar = 8.44e+11;
     char charVar = 'W';
+    float decimal = 3.9999393939220;
 
     // _Bool boolVar = 0;
     bool boolVar1 = true;
     bool boolVar0 = false;
 
-    printf("integerVar: %d\n floatingVar: %f\n doubleVar: %lf\n boolVar0: %d\n boolVar1: %d\n charVar: %c\n",
-           integerVar, floatingVar, doubleVar, boolVar0, boolVar1, charVar);
+    printf("integerVar: %d\n floatingVar: %f\n doubleVar: %lf\n boolVar0: %d\n boolVar1: %d\n charVar: %c\n decimal: %.5f\n",
+           integerVar, floatingVar, doubleVar, boolVar0, boolVar1, charVar, decimal);
 
     return 0;
 }
