e647489284ccaae7fda719b6d0209b52890c06ba
levilu800b <levilu800b@gmail.com>
2025-02-21 14:58:45 +0000

    char

--- Basics/Basics_2/VariablesAndDatatypes/Variables_Datatypes.c
+++ Basics/Basics_2/VariablesAndDatatypes/Variables_Datatypes.c
@@ -6,13 +6,14 @@ int main(void)
     int integerVar = 100;
     float floatingVar = 331.79;
     double doubleVar = 8.44e+11;
+    char charVar = 'W';
 
     // _Bool boolVar = 0;
     bool boolVar1 = true;
     bool boolVar0 = false;
 
-    printf("integerVar: %d\nfloatingVar: %f\ndoubleVar: %lf\nboolVar0: %d\nboolVar1: %d\n",
-           integerVar, floatingVar, doubleVar, boolVar0, boolVar1);
+    printf("integerVar: %d\n floatingVar: %f\n doubleVar: %lf\n boolVar0: %d\n boolVar1: %d\n charVar: %c\n",
+           integerVar, floatingVar, doubleVar, boolVar0, boolVar1, charVar);
 
     return 0;
 }
