967ba9493e87a6f26fb9d9e3b5e3646a0df5803c
levilu800b <levilu800b@gmail.com>
2025-02-28 12:45:52 +0000

    rectangle challenge

--- Basics/Basics_2/EnumsAndChars/challenge2.c
+++ Basics/Basics_2/EnumsAndChars/challenge2.c
@@ -2,6 +2,18 @@
 
 int main ()
 {
+    double width = 32.3;
+    double height = 15.3;
+    double perimeter;
+    double area;
+
+    perimeter = 2.0 * (height + width);
+
+    area = width * height;
+
+    printf("The perimeter of the rectangle is: %lf\n", perimeter);
+
+    printf("The area of the rectangle is: %lf\n", area);
     
     return 0;
 }
